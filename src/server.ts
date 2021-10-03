b773626873c464bb43398cf30f342ad8fb6063f2
Tobalase Akinyemi <funspectre@users.noreply.github.com>
2021-10-04 00:58:38 +0100

    Add image filtering endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,54 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import Joi from "joi";
+
+const urlSchema = Joi.string()
+  .trim()
+  .uri({
+    scheme: ["http", "https"]
+  });
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  // Endpoint to filter an image from a public url.
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req, res) => {
+    try {
+      // Sanitize and validate the image_url query parameter
+      const imageUrl = await urlSchema.validateAsync(req.query.image_url);
+      // Filter the image at the URL
+      const filterImagePath = await filterImageFromURL(imageUrl);
+      // Send the filtered image and delete the image after it is sent.
+      return res.status(200).sendFile(filterImagePath, () => {
+        deleteLocalFiles([filterImagePath]);
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(422).send({ message: "Invalid image_url parameter" });
+    }
+  });
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
